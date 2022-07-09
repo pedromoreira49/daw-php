533a0f4529bd15d4ecc12bc26dad020e8256fb04
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-07-09 00:26:16 -0300

    finalizado index da aplicacao

--- sem01/index.php
+++ sem01/index.php
@@ -3,9 +3,18 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="styles.css">
 	<title>sem 01</title>
 </head>
 <body>
-	<h1>hello world</h1>
+	<div class="form-style-6">
+		<h1>Search products</h1>
+		<form action="actions/procurarProdutos.php" method="post">
+			<input type="text" name="nome" placeholder="Pesquisar produtos" />
+			<input type="submit" value="Procurar" />
+			<input type="button" value="Cadastrar Categoria" onclick="window.location.href='pages/categorias.php'">
+			<input type="button" value="Cadastrar Produtos" onclick="window.location.href='pages/produtos.php'">
+		</form>
+	</div>
 </body>
 </html>
