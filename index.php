67fa3080658a4002f2cfa18277b659ff2c252ed9
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-07-19 22:17:40 -0300

    modified index to login page controll

--- index.php
+++ index.php
@@ -3,17 +3,16 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="styles.css">
+	<link rel="stylesheet" href="css/styles.css">
 	<title>sem 01</title>
 </head>
 <body>
 	<div class="form-style-6">
-		<h1>Search products</h1>
-		<form action="actions/procurarProdutos.php" method="post">
-			<input type="text" name="nome" placeholder="Pesquisar produtos" />
-			<input type="submit" value="Procurar" />
-			<input type="button" value="Cadastrar Categoria" onclick="window.location.href='pages/categorias.php'">
-			<input type="button" value="Cadastrar Produtos" onclick="window.location.href='pages/produtos.php'">
+		<h1>Login:</h1>
+		<form action="actions/login.php" method="post">
+			<input type="text" name="email" placeholder="Email" />
+			<input type="password" name="pass" placeholder="Senha" />
+			<input type="submit" value="Entrar" />
 		</form>
 	</div>
 </body>
